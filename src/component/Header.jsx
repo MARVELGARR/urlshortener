78d13a5ab8a1983fec73c638ac7462cba23f3036
MARVELGARR <75810831+MARVELGARR@users.noreply.github.com>
2023-05-05 21:46:32 +0100

    desktop view

--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -20,28 +20,28 @@ function Header() {
     }
 
   return (
-    <div className=''>
+    <div className='md:flex md:items-center'>
         <div className=" text-white text-center">
 
-            <div className="pl-5 flex justify-between mt-4">
+            <div className="md:mt-1 md:w-full md:h-24 md:justify-evenly   md:items-center pl-5 flex justify-between mt-4">
                 <img 
                 src="/images/logo.svg" 
-                className=" object-fit w-28 h-7"
+                className=" object-fit w-28 h-7 mt-2"
                 />
-                <div className={` ${isOpen ? "" : "hidden"} flex w-5/6 h-72 justify-center items-center min flex-col gap-3 text-xl absolute top-14 rounded-xl pt-5 pb-5 right-8`}>
+                <div className={`md:flex md:justify-between md:w-screen   md:text-black md:bg-transparent md:flex-row md:static    ${isOpen ? "lg:flex md:flex" : "lg:flex md:flex hidden"} flex w-5/6 h-72 justify-center items-center min flex-col gap-3 text-xl absolute top-14 rounded-xl pt-5 pb-5 right-8`}>
 
-                    <nav className={` flex flex-col gap-5 list-none`}>{header.nav.Links.map((items)=>{
+                    <nav className={`md:ml-16 md:flex-row flex flex-col gap-5 list-none`}>{header.nav.Links.map((items)=>{
                         return(
-                            <li className="flex cursor-pointer flex-col gap-3" key={uuid()}>
+                            <li className="hover:text-cyan-400 flex cursor-pointer flex-col gap-3" key={uuid()}>
                                 <div>{items}</div>
                             </li>
                         )
                     })}</nav>
                     <hr className="bg-white"/>
-                    <div className="flex flex-col gap-5">
+                    <div className="md:flex-row md:items-center md:pr-52 md:w-32 md:gap-5 flex flex-col gap-5">
                         {header.logs.Links.map((item)=>{
                             return(
-                                <div key={uuid()} className="cursor-pointer last:bg-cyan-400 last:p-3 last:rounded-3xl w-64  text-center">{item}</div>
+                                <div key={uuid()} className="md:last:pl-4 md:last:pb-1 md:last:pt-1 md:last:flex md:last:justify-center md:last:items-center md:last:pr-4 md:last:w-32 md:last:text-white   cursor-pointer last:bg-cyan-400 last:p-3 last:rounded-3xl w-64  text-center">{item}</div>
                             )
                         })}
                     </div>
@@ -49,12 +49,12 @@ function Header() {
 
             </div>
         </div>
-        <div className=" w-10 h-10 ">
+        <div className=" w-10 h-10 md:hidden lg:hidden">
             {isOpen ? (<button onClick={handleMenu}>
-                <img className="absolute h-10 w-10 top-2 right-10 " src="/images/close.svg"/>
+                <img className="absolute h-10 w-10 top-1 right-5 " src="/images/close.svg"/>
             </button>) :
             (<button onClick={handleMenu}>
-                <img className="absolute h-10 w-10 top-2 right-10" src="/images/open.svg"/>
+                <img className="absolute h-10 w-10 top-1 right-5" src="/images/open.svg"/>
             </button>)}
         </div>
     </div>
--- src/component/Hero.jsx
+++ src/component/Hero.jsx
@@ -60,25 +60,26 @@ function Hero() {
     }
 
   return (
-    <main className="flex gap-10 p-4 pr-4 flex-col justify-center items-center">
-        <section className="flex flex-col-reverse items-center">
-            <div className='flex flex-col items-center gap-5'>
-                <div className=" font-extrabold text-4xl text-center">{section1.h1}</div>
-                <div className="text-center text-lg text-gray-400 tracking-wider leading-7 mt-2">{section1.p}</div>
+    <main className=" md:p-20 bg-slate-300/50 flex gap-10 p-4 pr-4 flex-col justify-center items-center">
+        <section className="bg-white md:flex-row md:h-72     flex flex-col-reverse items-center">
+            <div className='md:items-start md:w-2/4  flex flex-col items-center gap-5'>
+                <div className="md:text-5xl md:text-left    font-extrabold text-4xl text-center">{section1.h1}</div>
+                <div className="md:text-left text-center text-lg text-gray-400 tracking-wider leading-7 mt-2">{section1.p}</div>
                 <div className=" bg-cyan-400 w-44 text-center p-2 rounded-xl text-lg font-bold text-white">{section1.btn1}</div>
             </div>
-            <img className="ml-28 w-96 " src={HeroImage} alt="Hero image"/>
+            <img className=" md:w-2/4 ml-28 w-96 " src={HeroImage} alt="Hero image"/>
         </section>
-        <section className="w-full h-aut0">
-            <div className="shorten-desktop hidden ">
-                <form 
+        <section className=" absolute w-full h-aut0">
+            <div className="md:block    shorten-desktop hidden ">
+                <form
+                    className="md:items-center md:justify-between  flex flex-row pl-5 pr-5 p-3 h-full gap-5" 
                     onSubmit={handleSubmit}
                 >
                     <input
                         type="text" 
                         value={text}
                         onChange={handleTyping} 
-                        className=" text-lg placeholder:ml-5"
+                        className="md:w-3/4 text-lg placeholder:ml-5 rounded-lg mt-1 mb-1 h-10 pl-5 "
                         placeholder='Shorten a link here...'
                     />
                     <div className=" text-red-900 italic">{message}</div>
@@ -86,7 +87,7 @@ function Hero() {
                 </form>
             </div>
             <div className='hidden'></div>
-            <div className='shorten-mobile h-full rounded-lg flex flex-col'>
+            <div className='md:hidden shorten-mobile h-full rounded-lg flex flex-col'>
                 <form
                     onSubmit={handleSubmit} 
                     className="flex flex-col pl-5 pr-5 p-3 h-full gap-5"
@@ -104,16 +105,19 @@ function Hero() {
                     <button type="submit" className="bg-cyan-400 cursor-pointer w-full h-10 text-center p-1 rounded-lg text-lg font-bold text-white">{mainApp.btn2}</button>
                 </form>
             </div>
-            <div className='bg-white flex flex-col gap7'>
+            <div className='md:mt-7    md:bg-inherit bg-white flex flex-col gap-7 '>
                 { list.map((item) => (
-                  item && item.L1 && (  <div key={item.code} className="flex flex-col gap-3 justify-center p-4">
-                        <div>{item.L1.slice(0, 40) + "..."}</div>
-                        <hr />
-                        <div className="text-cyan-400">{item.L2}</div>
+                  item && item.L1 && (  <div key={item.code} className=" md:bg-white   md:flex-row md:justify-evenly flex flex-col gap-3 justify-center p-4">
+                        <div className="md:flex md:gap-20 md:flex-row-reverse">
+
+                            <div>{item.L1}</div>
+                            <hr />
+                            <div className="text-cyan-400">{item.L2.slice(0, 40) + "..."}</div>
+                        </div>
                         <button
                             type="button"
                             onClick={()=>handleCopy(item.L1)} 
-                            className={`${coppied===item.L1 ? " bg-purple-950":"bg-cyan-400"} font-bold text-lg text-white flex items-center p-2 rounded-xl justify-center`}
+                            className={`md:w-44 md:rounded-lg    ${coppied===item.L1 ? " bg-purple-950":"bg-cyan-400"} font-bold text-lg text-white flex items-center p-2 rounded-xl justify-center`}
                         >{coppied === item.L1 ? "coppied!" : "copy"}</button>
                     </div> )
                 ))}
@@ -121,37 +125,37 @@ function Hero() {
         </section>
 
         <section className='h-aut0'>
-            <div className="flex flex-col gap-4">
+            <div className="md:pl-48 md:pr-48    flex flex-col gap-4">
                 <h1 className="text-3xl font-bold text-center">{section2.h1}</h1>
                 <p className="text-center text-lg text-gray-400 tracking-wider leading-7">{section2.p}</p>
             </div>
-            <div className="flex gap-16 flex-col justify-center items-center">
-                <article className='relative mt-10 text-center gap-5    flex flex-col justify-center items-center'>
+            <div className="md:flex-row md:mt-20    flex gap-24 flex-col justify-center items-center">
+                <article className='md:mb-10 md:p-5   relative pt-10 pb-10 bg-white mt-10 text-center gap-5    flex flex-col justify-center items-center'>
                     <img
                     src={section2.articles[0].img} 
                     className="articleImg " />
                     <div className="">
-                        <div className="">{section2.articles[0].h3}</div>
+                        <div className=" font-bold text-lg">{section2.articles[0].h3}</div>
                         <div className="">{section2.articles[0].p}</div>
 
                     </div>
                 </article>
-                <article className='relative text-center gap-5 flex flex-col justify-center items-center'>
+                <article className='md:mt-20 md:p-5   relative pt-10 pb-10 bg-white text-center gap-5 flex flex-col justify-center items-center'>
                     <img
                     src={section2.articles[1].img} 
                     className="articleImg" />
                     <div className="">
-                        <div className="me">{section2.articles[1].h3}</div>
+                        <div className="lol font-bold text-lg"><span className="me  hidden md:block md:-left-12 md:top-10 md:rotate-90"></span>{section2.articles[1].h3}</div>
                         <div className="">{section2.articles[1].p}</div>
 
                     </div>
                 </article>
-                <article className='relative text-center gap-5 flex flex-col justify-center items-center'>
+                <article className='md:mt-32 md:p-5  relative pt-10 pb-10 bg-white text-center gap-5 flex flex-col justify-center items-center'>
                     <img
                     src={section2.articles[2].img} 
                     className="articleImg" />
                     <div className="">
-                        <div className="me">{section2.articles[2].h3}</div>
+                        <div className="font-bold text-lg"><span className='me hidden md:block md:-left-12 md:top-20 md:rotate-90'></span>{section2.articles[2].h3}</div>
                         <div className="">{section2.articles[2].p}</div>
 
                     </div>
